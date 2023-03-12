720da01d0106ec3624702706c9d4be0628ba072d
unknown <bhumithakur1218@gmail.com>
2023-03-12 14:18:20 +0530

    fc

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -6,7 +6,7 @@ import '../index.css';
 function Card() {
     return (
       <div className="absolute w-334 h-655 left-165 top-800 bg-gray-100 shadow-md rounded-lg">
-       
+       <h1>cards</h1>
       </div>
     );
   }
